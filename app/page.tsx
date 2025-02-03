a6e13150ef13e138683a1f1cf92435ee91c571fa
Gibeom Choi <kcdevdes@gmail.com>
2025-02-03 11:37:31 -0500

    Refactor: Delete API Dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  // get fetch data from 'localhost:8080/api/v1/greeting'
-  // and declare it as data
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/api/v1/greeting')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  });
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -38,7 +26,7 @@ export default function Home() {
         </ol>
 
         <h2 className="text-2xl sm:text-3xl font-semibold text-center sm:text-left">
-          {data}
+          The world is your oyster.
         </h2>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
